5cab8108934b70b9432237fa29289f350095abba
bamna7659-svg <bamna7659@gmail.com>
2026-07-06 08:44:45 +0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if user_input:
     # GROQ AI RESPONSE
     # ----------------------------
     response = client.chat.completions.create(
-    model="llama3-8b-8192",
+    model="llama-3.1-8b-instant",
     messages=st.session_state.messages
 )
 
